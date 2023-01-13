172df53a8280c84fd791b997c4cee4cc9f25124f
jdcheek <58921502+jdcheek@users.noreply.github.com>
2023-01-12 23:08:04 -0600

    added turbo & importmap, user nav buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # get 'home/index'
+  devise_for :users
+  resources :comments
   get 'home/about'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
